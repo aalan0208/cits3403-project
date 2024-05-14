712aefd343756360eb5c7bb4ed74b9d8c330061b
TomTran11 <quangthong11@gmail.com>
2024-05-14 21:56:56 +0800

    Relocate file for flask to work and change external file pathway included

---
+++ static/quiz_page.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('#account').click(function(){
+        $('#sub_panel').toggleClass('d-none');
+        $('#accountInfo').toggleClass('d-none');
+    });
+
+    $('#setting').click(function(){
+        $('#setting_detail').toggleClass('d-none');
+    });
+
+    $('#setting_detail').click(function(){
+        $('body').toggleClass('dark-background');
+    });
+
+    $('#logout').click(function(){
+        $('#yesno').toggleClass('d-none');
+        $('#setting_detail').addClass('d-none');
+        $('#about_contact').addClass('d-none');
+    });
+
+    $('#no').click(function(){
+        $('#yesno').toggleClass('d-none');
+    })
+
+    $('#yes').click(function(){
+        // Perform logout here
+
+        // Redirect to the login page
+        window.location.href = 'Login.html';       
+    });
+
+    $('#about').click(function(){
+        $('#about_contact').toggleClass('d-none');
+    });
+});
