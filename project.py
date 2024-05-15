528048e7c1b0ca8845e416b65659f26d67e2c7a5
Namgay Choden <164040031+Nchoden@users.noreply.github.com>
2024-05-15 13:39:15 +0800

    deleted render template

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template
 import random
 import time
 from sqlalchemy.exc import IntegrityError
