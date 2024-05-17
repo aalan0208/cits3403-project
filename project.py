0826e992465027ea2183a09a376b4e6cd2daef3e
Nchoden <23867057@student.uwa.edu.au>
2024-05-18 00:05:32 +0800

    password hashing

--- project.py
+++ project.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import random
 import time
 from sqlalchemy.exc import IntegrityError
-
+from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Change this to a secure secret key
+app.secret_key = "your_secret_key"  # Change this to a secure secret key(NEEDS TO BE DONE AT THE ENDDD)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///quizlet.db"  # SQLite database URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
@@ -53,7 +53,9 @@ class Result(db.Model):
 def setup_database():
     with app.app_context():
         db.create_all()
-
+if __name__ == "__main__":
+    setup_database()
+    app.run(debug=True)
 
 # Login route
 @app.route("/login", methods=["GET", "POST"])
@@ -63,7 +65,7 @@ def login():
         password = request.form["password"]
         user = User.query.filter_by(email=email).first()
 
-        if user and user.password == password:
+        if user and check_password_hash(user.password, password):
             session["user_id"] = user.id
             flash("Logged in successfully!", "success")
             return redirect(url_for("dashboard"))
@@ -95,7 +97,8 @@ def signup():
             return redirect(url_for("login"))
 
         try:
-            new_user = User(email=email, password=password)
+            hashed_password = generate_password_hash(password, method='sha256')
+            new_user = User(email=email, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             flash("Account created successfully! Please log in.", "success")
